ef5b09a4821011b339d3c135eda4c0518fb163bc
Rohan Jeendgar <coolbirb2309@gmail.com>
2023-02-16 13:06:27 +0530

    Makeshift button for sorting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Box, Button, ImageList, ImageListItem, Menu, MenuItem, Modal, Snackbar } from "@mui/material"
+import { Box, Button, ImageList, ImageListItem, Menu, MenuItem, MenuList, Modal, Snackbar } from "@mui/material"
 import SignIn from "components/SignIn"
 import { signOut, useSession } from "next-auth/react"
 import React, { useEffect } from "react";
@@ -9,16 +9,25 @@ import { getImageData } from "./api/image/getImage";
 
 const Home = (props) => {
   let { data: session, status } = useSession()
-  // let [images, setImages] = React.useState([]);
-  let images = JSON.parse(props.images);
-  // useEffect(() => {
-  //   const getImages = async () => {
-  //     let imagesReq = await fetch("/api/image/getImage")
-  //     let newImages = await imagesReq.json()
-  //     setImages(newImages)
-  //   }
-  //   getImages();
-  // }, [])
+  let [images, setImages] = React.useState([]);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  // let images = JSON.parse(props.images);
+  useEffect(() => {
+    // const getImages = async () => {
+    //   let imagesReq = await fetch("/api/image/getImage")
+    //   let newImages = await imagesReq.json()
+    //   setImages(newImages)
+    // }
+    // getImages();
+    setImages(JSON.parse(props.images))
+  }, [])
 
   return (
     <Box className={styles.container}>
@@ -26,6 +35,39 @@ const Home = (props) => {
         <SignIn/>
        :
        <>
+         <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+         >
+           Sort
+         </Button>
+         <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+            'aria-labelledby': 'basic-button',
+            }}
+         >
+           <MenuItem onClick={() => {
+             if(localStorage.getItem("sort") !== "up-lt") {
+                setImages(images.reverse())
+                localStorage.setItem("sort", "up-lt")
+             } return;
+           }}>
+             By upload date: Latest First
+           </MenuItem>
+           <MenuItem onClick={() => {
+             if(localStorage.getItem("sort") !== "up-od"){
+               setImages(images.reverse())
+               localStorage.setItem("sort", "up-od")
+             } return;
+           }}>By upload date: Oldest First</MenuItem>
+         </Menu>
          <ImageView images={images}/>
        </>
       }
